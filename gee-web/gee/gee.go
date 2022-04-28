8031eed208d13b52349498fef713416fd7c15900
sk <kaixuan.sun@boonray.com>
2022-04-28 14:24:25 +0800

    完成分组控制

--- gee-web/gee/gee.go
+++ gee-web/gee/gee.go
@@ -1,39 +1,53 @@
 package gee
 
 import (
+	"log"
 	"net/http"
 )
 
 // 定义请求处理程序
 type HandlerFunc func(*Context)
 
+// 分组控制
+type RouterGroup struct {
+	prefix      string
+	middlewares []HandlerFunc // 中间件
+	parent      *RouterGroup
+	engine      *Engine
+}
+
 // 定义服务引擎
 type Engine struct {
-	router *router // 路由映射表
+	*RouterGroup                // 继承
+	router       *router        // 路由映射表
+	groups       []*RouterGroup // 存储所有组
 }
 
 // 创建引擎函数
 func New() *Engine {
-	return &Engine{router: newRouter()}
+	engine := &Engine{router: newRouter()}
+	engine.RouterGroup = &RouterGroup{engine: engine}
+	engine.groups = []*RouterGroup{engine.RouterGroup}
+	return engine
 }
 
 /*
 定义引擎对象功能
 */
-// 添加路由
-func (engine *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
-	engine.router.addRoute(method, pattern, handler)
-}
+// // 添加路由
+// func (engine *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
+// 	engine.router.addRoute(method, pattern, handler)
+// }
 
-// 定义get方法
-func (engine *Engine) GET(pattern string, handler HandlerFunc) {
-	engine.addRoute("GET", pattern, handler)
-}
+// // 定义get方法
+// func (engine *Engine) GET(pattern string, handler HandlerFunc) {
+// 	engine.addRoute("GET", pattern, handler)
+// }
 
-// 定义post方法
-func (engine *Engine) POST(pattern string, handler HandlerFunc) {
-	engine.addRoute("POST", pattern, handler)
-}
+// // 定义post方法
+// func (engine *Engine) POST(pattern string, handler HandlerFunc) {
+// 	engine.addRoute("POST", pattern, handler)
+// }
 
 //定义运行服务
 func (engine *Engine) Run(addr string) error {
@@ -44,4 +58,37 @@ func (engine *Engine) Run(addr string) error {
 func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	c := newContext(w, req)
 	engine.router.handle(c)
+}
+
+/*
+定义路由组功能
+*/
+
+// 添加组
+func (group *RouterGroup) Group(prefix string) *RouterGroup {
+	engine := group.engine
+	newGroup := &RouterGroup{
+		prefix: group.prefix + prefix,
+		parent: group,
+		engine: engine,
+	}
+	engine.groups = append(engine.groups, newGroup)
+	return newGroup
+}
+
+// 添加路由
+func (group *RouterGroup) addRoute(method string, comp string, handler HandlerFunc) {
+	pattern := group.prefix + comp
+	log.Printf("Route %4s - %s", method, pattern)
+	group.engine.router.addRoute(method, pattern, handler)
+}
+
+// GET defines the method to add GET request
+func (group *RouterGroup) GET(pattern string, handler HandlerFunc) {
+	group.addRoute("GET", pattern, handler)
+}
+
+// POST defines the method to add POST request
+func (group *RouterGroup) POST(pattern string, handler HandlerFunc) {
+	group.addRoute("POST", pattern, handler)
 }
--- gee-web/main.go
+++ gee-web/main.go
@@ -8,23 +8,34 @@ import (
 
 func main() {
 	r := gee.New()
-	r.GET("/", func(c *gee.Context) {
-		c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
+	r.GET("/index", func(c *gee.Context) {
+		c.HTML(http.StatusOK, "<h1>Index Page</h1>")
 	})
+	v1 := r.Group("/v1")
+	{
+		v1.GET("/", func(c *gee.Context) {
+			c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
+		})
 
-	r.GET("/hello", func(c *gee.Context) {
-		// expect /hello?name=geektutu
-		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
-	})
+		v1.GET("/hello", func(c *gee.Context) {
+			// expect /hello?name=geektutu
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+		})
+	}
+	v2 := r.Group("/v2")
+	{
+		v2.GET("/hello/:name", func(c *gee.Context) {
+			// expect /hello/geektutu
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+		})
+		v2.POST("/login", func(c *gee.Context) {
+			c.JSON(http.StatusOK, gee.H{
+				"username": c.PostForm("username"),
+				"password": c.PostForm("password"),
+			})
+		})
 
-	r.GET("/hello/:name", func(c *gee.Context) {
-		// expect /hello/geektutu
-		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
-	})
-
-	r.GET("/assets/*filepath", func(c *gee.Context) {
-		c.JSON(http.StatusOK, gee.H{"filepath": c.Param("filepath")})
-	})
+	}
 
 	r.Run(":9999")
 }
